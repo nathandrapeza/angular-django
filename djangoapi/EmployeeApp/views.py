b9641ff3c2278a59dd985ec0a3d6c9bd13ca47ff
nathandrapeza <drapeza@ualberta.ca>
2022-03-03 15:50:31 -0700

    added models and partial serializers

--- djangoapi/EmployeeApp/views.py
+++ djangoapi/EmployeeApp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.parsers import JSONParser
+# allows other domains to acces our api methods
+# (our front-end project)
+from django.views.decorators.csrf import csrf_exempt
+from EmployeeApp.models import Departments, Employees
+from EmployeeApp.serializers import DepartmentSerializer, EmployeeSerializer
+from fjango.http.response import JsonResponse
 
 # Create your views here.
+@csrf_exempt
+def departmentApi(request, id=0):
+    if request.method == 'GET':
+        departments = Departments.objects.all()
+        departments_serializer = DepartmentSerializer(departments, many=True)
+        return JsonResponse(departments_serializer.data, safe=False)
+    elif request.method == 'POST':
+        # using JSONParser for input
+        department_data = JSONParser().parse(request)
